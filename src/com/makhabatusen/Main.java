58d0df6c4bbc91a503cabd7312fa3a28ac71250c
usenalieva <mahusya12@mail.ru>
2021-04-09 01:58:59 +0600

    initial commit

--- src/com/makhabatusen/Main.java
+++ src/com/makhabatusen/Main.java
@@ -3,6 +3,7 @@ package com.makhabatusen;
 public class Main {
 
     public static void main(String[] args) {
+        new ATM().withdraw();
         new Bonus().calcBonus();
         new Words().scanWord();
         new Addition().add();
